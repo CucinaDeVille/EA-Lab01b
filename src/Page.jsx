047062478b566b90f7d2db18e323aef078172c22
luca <luca.romano@t-online.de>
2026-05-03 14:38:06 +0200

    feat(page): :technologist: simplify structure of component page

--- src/Page.jsx
+++ src/Page.jsx
@@ -3,15 +3,13 @@ import Article from "./Article";
 export default function Page({ content }) {
     return (
         <div>
-            {(() => {
-                return content.map((item, index) => (
-                    <Article
-                        key={index}
-                        {...item}
-                        mynum={index + 1}
-                    />
-                ));
-            })()}
+            {content.map((item, index) => (
+                <Article
+                    key={index}
+                    {...item}
+                    mynum={index + 1}
+                />
+            ))}
         </div>
     );
 }
